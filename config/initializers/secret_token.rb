292f8c02fe449b57f588ed7d4c0f54a457e5b3e1
jeremiahishere <jeremiah@cloudspace.com>
2011-08-24 22:04:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperjsTutorial::Application.config.secret_token = '0be8554efea853b5d213803942caa2e760cc2b417ce170bf03d755d96dcf7576f5d4bb6bc1028f2b81092b96f0da02b6be32dfa5fa97e84b86e4ebc34146c6d0'
