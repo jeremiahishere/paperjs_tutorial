1756709925240978ab4c963099e4e0342e693f20
jeremiahishere <jeremiah@cloudspace.com>
2011-08-24 23:19:41 -0400

    Setup basic tutorial structure

---
+++ app/controllers/tutorials_controller.rb
@@ -0,0 +1,25 @@
+class TutorialsController < ApplicationController
+
+  # creating a point
+  # creating a size
+  # using a point or size to create a rectangle
+  def part_one
+  end
+
+  # circle, rounded rectangle, various sized polygons
+  # set selected to true at some point
+  def part_two
+  end
+
+  # stroke style, fill color, opacity
+  def part_three
+  end
+
+  # animation example using onFrame
+  def part_four
+  end
+
+  # objects
+  def part_five
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,11 @@
 PaperjsTutorial::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  root :to => "pages#index"
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match "tutorials/one" => "tutorials#part_one", :as => "tutorial_part_one"
+  match "tutorials/two" => "tutorials#part_two", :as => "tutorial_part_two"
+  match "tutorials/three" => "tutorials#part_three", :as => "tutorial_part_three"
+  match "tutorials/four" => "tutorials#part_four", :as => "tutorial_part_four"
+  match "tutorials/five" => "tutorials#part_five", :as => "tutorial_part_five"
+  match "tutorials/six" => "tutorials#part_six", :as => "tutorial_part_six"
+  match "tutorials/seven" => "tutorials#part_seven", :as => "tutorial_part_seven"
 end
